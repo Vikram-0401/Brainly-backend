b9d56c48e116ba0f28c957ee0a73615c44b49df8
vikram <vikramshettyr4@gmail.com>
2025-08-02 08:36:54 +0530

    Auth and content tested

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import mongoose, {model, Schema} from "mongoose";
+
+mongoose.connect("")
+
+const UserSchema = new Schema({
+    username : {type: String, unique: true},
+    password : String
+})
+
+export const UserModel = model("Users", UserSchema);
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
+})
+
+export const ContentModel = model("Content", ContentSchema);
