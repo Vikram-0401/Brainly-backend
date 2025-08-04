c80101eec5f7a3a409a8276230fde83742cf2d2a
vikram <vikramshettyr4@gmail.com>
2025-08-04 15:03:51 +0530

    Updated schema

--- src/db.ts
+++ src/db.ts
@@ -13,6 +13,7 @@ const ContentSchema = new Schema({
     title: String,
     link: String,
     tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    type: String,
     userId: {type: mongoose.Types.ObjectId, ref: 'Users', required: true}
 })
 
